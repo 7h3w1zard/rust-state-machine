73f3ff6c964a19ecab8d952a5f1fc5e44a443712
7h3w1zard <7h3w1zard@gmail.com>
2026-09-18 17:51:52 +0300

    Adding call macro

--- src/balances.rs
+++ src/balances.rs
@@ -14,29 +14,16 @@ pub struct Pallet<T: Config> {
     balances: BTreeMap<T::AccountId, T::Balance>,
 }
 
+#[macros::call]
 impl<T: Config> Pallet<T> {
-    /// Create a new instance of the balances module.
-    pub fn new() -> Self {
-        Self {
-            balances: BTreeMap::new(),
-        }
-    }
-    /// Set the balance o an account `who` to some `amount`.
-    pub fn set_balance(&mut self, who: &T::AccountId, amount: T::Balance) {
-        self.balances.insert(who.clone(), amount);
-    }
-    /// Get the balnce of an account `who`.
-    /// If the account has no stored balance, we return zero(0).
-    pub fn balance(&self, who: &T::AccountId) -> T::Balance {
-        *self.balances.get(who).unwrap_or(&T::Balance::zero())
-    }
+    
     /// Transfer `amount` from one account to another.
     /// This function verifies thet `from` has at least `amount` balance to transfer,
     /// and that no mathematical overflows occur.
     pub fn transfer(
         &mut self,
-        caller: &T::AccountId,
-        to: &T::AccountId,
+        caller: T::AccountId,
+        to: T::AccountId,
         amount: T::Balance,
     ) -> Result<(), &'static str> {
         let from_balance = self.balance(&caller);
@@ -56,34 +43,21 @@ impl<T: Config> Pallet<T> {
     }
 }
 
-// A public enum which desribes the calls we want
-// to expose to the dispatcher.
-// We should expect that the caller of each call will be provided
-// by the dispatcher, and not included as a parameter of the call.
-pub enum Call<T: Config> {
-    Transfer {
-        to: T::AccountId,
-        amount: T::Balance,
-    },
-}
-
-/// Implementation of the dispatch logic, mapping from `BalancesCall`
-/// to the approriate underlying function we want to execute.
-impl<T: Config> crate::support::Dispatch for Pallet<T> {
-    type Caller = T::AccountId;
-    type Call = Call<T>;
-
-    fn dispatch(
-        &mut self,
-        caller: Self::Caller,
-        call: Self::Call,
-    ) -> crate::support::DispatchResult {
-        match call {
-            Call::Transfer { to, amount } => {
-                self.transfer(&caller, &to, amount)?;
-            }
+impl<T: Config> Pallet<T> {
+    /// Create a new instance of the balances module.
+    pub fn new() -> Self {
+        Self {
+            balances: BTreeMap::new(),
         }
-        Ok(())
+    }
+    /// Set the balance o an account `who` to some `amount`.
+    pub fn set_balance(&mut self, who: &T::AccountId, amount: T::Balance) {
+        self.balances.insert(who.clone(), amount);
+    }
+    /// Get the balnce of an account `who`.
+    /// If the account has no stored balance, we return zero(0).
+    pub fn balance(&self, who: &T::AccountId) -> T::Balance {
+        *self.balances.get(who).unwrap_or(&T::Balance::zero())
     }
 }
 
@@ -120,11 +94,11 @@ mod tests {
         let from = "alice".to_string();
         let to = "bob".to_string();
         let _set_balance = balances.set_balance(&from, 666);
-        let _fail_transfer = balances.transfer(&from, &to, 667);
+        let _fail_transfer = balances.transfer(from.clone(), to.clone(), 667);
         assert_eq!(balances.balance(&from), 666);
         assert_eq!(balances.balance(&to), 0);
 
-        let _success_transfer = balances.transfer(&from, &to, 555);
+        let _success_transfer = balances.transfer(from.clone(), to.clone(), 555);
         assert_eq!(balances.balance(&from), 111);
         assert_eq!(balances.balance(&to), 555);
     }
